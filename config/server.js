cf65de3a2145cef9001aecf744eea78207c42da5
lily <89727355+kitsuneremi@users.noreply.github.com>
2023-11-14 17:19:43 +0700

    save chat msg to mongodb

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io')
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const liveChats = require("../model/liveComments");
+mongoose.connect("mongodb+srv://lily:lily@lily.nqsymng.mongodb.net/lily");
 
 app.use(
     express.urlencoded({
@@ -15,22 +18,35 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://www.erinasaiyukii.com', ''],
-        methods: ['GET', 'POST']
-    }
-})
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "https://www.erinasaiyukii.com",
+            "https://erinasaiyukii.com",
+        ],
+        methods: ["GET", "POST"],
+    },
+});
 
-io.on('connection', socket => {
-    console.log(socket.id)
+io.on("connection", (socket) => {
+    console.log(socket.id);
 
-    socket.on('join', data => {
-        console.log(`id ${data.id} joined room ${data.room}`)
-        socket.join(data.room)
-    })
+    socket.on("join", (data) => {
+        console.log(`id ${data.id} joined room ${data.room}`);
+        socket.join(data.room);
+    });
 
-    socket.on('sendmsg', data => {
-        console.log(data)
-        socket.to(data.room).emit('rcvmsg', data)
-    })
-})
-server.listen(6074)
+    socket.on("sendmsg", (data) => {
+        socket.to(data.room).emit("rcvmsg", data);
+        liveChats.create({
+            accountId: data.accountId,
+            name: data.name,
+            image: data.image,
+            content: data.content,
+            room: data.room,
+        });
+    });
+});
+server.listen(6074, () => {
+    console.log("server started");
+});
