af00e6e1fc0bca212bda49461638544a9a09fa36
lily <89727355+kitsuneremi@users.noreply.github.com>
2024-01-22 05:16:00 +0700

    add chat server

--- config/server.js
+++ config/server.js
@@ -5,16 +5,16 @@ const http = require("http");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const liveChats = require("../model/liveComments");
+
 mongoose.connect("mongodb+srv://lily:lily@lily.nqsymng.mongodb.net/lily");
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
+app.use(express.urlencoded({
+    extended: true,
+}));
 
 app.use(cors());
 app.use(express.json());
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -31,25 +31,55 @@ const io = new Server(server, {
     },
 });
 
-io.on("connection", (socket) => {
-    console.log(socket.id);
+// Middleware to handle /live route
+app.use("/live", (req, res, next) => {
+    io.on("connection", (socket) => {
+        console.log(`[Live] New connection: ${socket.id}`);
 
-    socket.on("join", (data) => {
-        console.log(`id ${data.id} joined room ${data.room}`);
-        socket.join(data.room);
-    });
+        socket.on("join", (data) => {
+            console.log(`[Live] id ${data.id} joined room ${data.room}`);
+            socket.join(data.room);
+        });
 
-    socket.on("sendmsg", (data) => {
-        socket.to(data.room).emit("rcvmsg", data);
-        liveChats.create({
-            accountId: data.accountId,
-            name: data.name,
-            image: data.image,
-            content: data.content,
-            room: data.room,
+        socket.on("sendmsg", (data) => {
+            socket.to(data.room).emit("rcvmsg", data);
+            liveChats.create({
+                accountId: data.accountId,
+                name: data.name,
+                image: data.image,
+                content: data.content,
+                room: data.room,
+            });
         });
     });
+
+    next();
 });
+
+// Middleware to handle /chat route
+app.use("/chat", (req, res, next) => {
+    io.on("connection", (socket) => {
+        console.log(`[Chat] New connection: ${socket.id}`);
+
+        // Handle chat-specific logic here
+
+        // Example: Join a chat room
+        socket.on("join", (room) => {
+            console.log(`[Chat] ${socket.id} joined chat room ${room}`);
+            socket.join(room);
+        });
+
+        // Example: Handle chat messages
+        socket.on("sendMessage", (message) => {
+            console.log(`[Chat] Received message: ${message}`);
+            // Process and broadcast the message
+            io.to("chatRoom").emit("message", message);
+        });
+    });
+
+    next();
+});
+
 server.listen(6074, () => {
-    console.log("server started");
+    console.log("Server started");
 });
